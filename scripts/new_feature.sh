bfda9126b24a0b3205a04c35ac97ec00c3ba19fa
NicolasRichel <nicolasrichel.github@gmail.com>
2019-05-08 14:32:51 +0200

    (chore) : update new_feature script (need to be tested)

--- scripts/new_feature.sh
+++ scripts/new_feature.sh
@@ -2,8 +2,8 @@
 
 # Check that we are on develop branch
 CURRENT_BRANCH=`git rev-parse --abbrev-ref HEAD`
-if [ $CURRENT_BRANCH != "develop" ]; then
-  echo -e "\nYou must be on develop branch to create a new feature.\n"
+if [ "$CURRENT_BRANCH" != "develop" ]; then
+  echo -e "\nYou must start from develop branch to create a new feature branch.\n"
   exit 1
 fi
 
@@ -11,13 +11,14 @@ fi
 git fetch origin develop
 c1=`git rev-parse HEAD`
 c2=`git rev-parse FETCH_HEAD`
-if [ $c1 != $c2 ]; then
-  echo -e "\nYour local and remote branches are not synchronized.\n"
+if [ "$c1" != "$c2" ]; then
+  echo -e "\nYour local and remote develop branches are not synchronized."
+  echo -e "You must synchronize them (git pull) before before creating a new feature branch.\n"
   exit 1
 fi
 
 # Create local feature branch and push it to the remote
-FEATURE_BRANCH="feature/$1"
+FEATURE_BRANCH=feature/$1
 git checkout -b $FEATURE_BRANCH
 git push -u origin $FEATURE_BRANCH
 
